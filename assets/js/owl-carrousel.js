a3a141003ab49ea173364082ba3e3f1a6699d9d8
CarlaMGaldino <carlinhamg20@gmail.com>
2022-04-22 16:52:03 -0300

    Construção das seções, contato, blog, banners

---
+++ assets/js/owl-carrousel.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+  items: 4,
+  loop: false,
+  center: true,
+  margin: 10,
+  URLhashListener: true,
+  autoplayHoverPause: true,
+  startPosition: "URLHash",
+});
