c66bda7e18500132db663e09cc815494c03b9daf
CarlaMGaldino <carlinhamg20@gmail.com>
2022-04-26 19:11:56 -0300

    Construçao footer, ajustes de imagens, contrução owl carousel

--- assets/js/owl-carrousel.js
+++
@@ -1,9 +0,0 @@
-$(".owl-carousel").owlCarousel({
-  items: 4,
-  loop: false,
-  center: true,
-  margin: 10,
-  URLhashListener: true,
-  autoplayHoverPause: true,
-  startPosition: "URLHash",
-});
